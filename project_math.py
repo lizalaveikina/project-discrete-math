d66566f42c35a2cbc3d4bdeb7a2dd721ed0e1bca
lizalaveikina <laveikina.pn@ucu.edu.ua>
2022-12-23 13:33:42 +0200

    save

--- project_math.py
+++ project_math.py
@@ -69,4 +69,91 @@ def product(*iterates):
 # for i in product((1, 2, 4), (4, 6)):
 #     print(i, end=' ')
 # *********************************
+# Task 5
+def gen_perm(iterable,lenn=0):
+    if lenn==0 or lenn>len(iterable): lenn=len(iterable)
+    if lenn == 1:
+        for i in iterable:
+            yield i
+    else:
+        perms = gen_perm(iterable,lenn-1)
+        for i in perms:
+            yield i
+            if len(i) == lenn-1:
+                for j in iterable:
+                    if i!=j:
+                        yield i+j
+poem = []
+for i in gen_perm("ABCD",2):
+    poem.append(i)
+for k in poem:
+    if len(k) == 1:
+        poem.remove(k)
+print(poem)
+# *********************************
+# Task 6
+def combinations(iterable, r):
+    """
+    (str, int) -> Iterator[Tuple[int]]
+    Get combinations of iterable
+    >>> list(combinations('ABCD', 0))
+    [()]
+    >>> list(combinations('ABCD', 1))
+    [('A',), ('B',), ('C',), ('D',)]
+    >>> list(combinations('ABCD', 2))
+    [('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'C'), ('B', 'D'), ('C', 'D')]
+    >>> list(combinations('ABCD', 3))
+    [('A', 'B', 'C'), ('A', 'B', 'D'), ('A', 'C', 'D'), ('B', 'C', 'D')]
+    >>> list(combinations('ABCD', 4))
+    [('A', 'B', 'C', 'D')]
+    """
+    s = tuple(iterable)
+    n = len(iterable)
+    if r > n:
+        return
+    elif len(iterable) == r:
+        lst = range(r)
+        yield tuple(s[i] for i in lst)
+    elif r == 0:
+        yield ()
+    else:
+        yield tuple(iterable[i] for i in range(r))
+        lst = list(range(1, r+1))
+        while lst[0] != n - r + 1:
+            i = r
+            while lst[i - 1] == n - r + i:
+                i -= 1
+            lst[i - 1:] = list(range(lst[i - 1] + 1, lst[i - 1] + r - i + 2))
+            yield tuple(iterable[i - 1] for i in lst)
 
+
+# *********************************
+# Task 7
+def combinations_with_replacement(iterable, r):
+    """
+    Get combinations of iterable, with replacements
+    >>> list(combinations_with_replacement('', 5))
+    []
+    >>> list(combinations_with_replacement('ABCD', 0))
+    [()]
+    >>> list(combinations_with_replacement('ABCD', 1))
+    [('A',), ('B',), ('C',), ('D',)]
+    >>> list(combinations_with_replacement('ABCD', 2))
+    [('A', 'A'), ('A', 'B'), ('A', 'C'), ('A', 'D'), ('B', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'C'), ('C', 'D'), ('D', 'D')]
+    >>> list(combinations_with_replacement('ABCD', 3))
+    [('A', 'A', 'A'), ('A', 'A', 'B'), ('A', 'A', 'C'), ('A', 'A', 'D'), ('A', 'B', 'B'), ('A', 'B', 'C'), ('A', 'B', 'D'), ('A', 'C', 'C'), ('A', 'C', 'D'), ('A', 'D', 'D'), ('B', 'B', 'B'), ('B', 'B', 'C'), ('B', 'B', 'D'), ('B', 'C', 'C'), ('B', 'C', 'D'), ('B', 'D', 'D'), ('C', 'C', 'C'), ('C', 'C', 'D'), ('C', 'D', 'D'), ('D', 'D', 'D')]
+    """
+    n = len(iterable)
+    if r == 0:
+        yield ()
+    elif n == 0:
+        return
+    else:
+        lst = [1] * r
+        yield tuple(iterable[i - 1] for i in lst)
+        while lst[0] != n:
+            i = r - 1
+            while i > 0 and lst[i] == n:
+                i -= 1
+            lst[i:] = [lst[i] + 1] * (r - i)
+            yield tuple(iterable[i - 1] for i in lst)
